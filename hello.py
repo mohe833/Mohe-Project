219b0ce2eb6ab4bd5a88a3196cc574c0cc1f102f
mohe833 <mohammedkairu246@gmail.com>
2025-10-15 23:49:52 +0200

    Add myhello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,128 @@
-print("hello man of the match")
-user = input("What is your name? ")
-print("Welcome", user)
+print("ሰላም")
+print("አመሰግናለሁ!🙏🏻😁🔥 ሞሄ ነኝ✌️")
+print("The program is just for test")
+print("please to end the program use letter (q)")
+print("............Here you go.............")
+print("*" * 20)
+
+while True:
+   b = "The capital of"
+   a = input("Enter the country name: ").capitalize()
+   if a == "Ethiopia":
+      print(f"{b} {a} is Addis Ababa")
+      print("Ethiopia is the most beautifull country in Africa🔥🫶🏻😍")
+      print("ግድ ነዋ 😅😁🙆‍♂️")      
+   elif a == "Eritrea":
+      print(f"{b} {a} is Asmera.")
+   elif a == "Somalia":
+      print(f"{b} {a} is Mogadishu.")
+   elif a == "Kenya":
+      print(f"{b} {a} is Nairobi.")
+   elif a == "Egypt":
+      print(f"{b} {a} is Cairo.")
+   elif a == "Tanzania":
+       print(f"{b} {a} is Dodoma.")
+   elif a == "Mali":
+       print(f"{b} {a} is Bamako")
+   elif a == "Angola.":
+       print(f"{b} {a} is Luanda.")
+   elif a.lower() == "q":
+       break
+   elif a == "Algeria":
+    print(f"{b} {a} is Algiers.")
+   elif a == "Angola":
+    print(f"{b} {a} is Luanda.")
+   elif a == "Benin":
+    print(f"{b} {a} is Porto-Novo.")
+   elif a == "Botswana":
+    print(f"{b} {a} is Gaborone.")
+   elif a == "Burkina Faso":
+    print(f"{b} {a} is Ouagadougou.")
+   elif a == "Burundi":
+    print(f"{b} {a} is Gitega.")
+   elif a == "Cabo Verde (Cape Verde)":
+    print(f"{b} {a} is Praia.")
+   elif a == "Cameroon":
+    print(f"{b} {a} is Yaoundé.")
+   elif a == "Central African Republic":
+    print(f"{b} {a} is Bangui.")
+   elif a == "Chad":
+    print(f"{b} {a} is N’Djamena.")
+   elif a == "Comoros":
+    print(f"{b} {a} is Moroni.")
+   elif a == "Congo (Republic of)":
+    print(f"{b} {a} is Brazzaville.")
+   elif a == "Congo (Democratic Republic)":
+    print(f"{b} {a} is Kinshasa.")
+   elif a == "Côte d’Ivoire (Ivory Coast)":
+    print(f"{b} {a} is Yamoussoukro.")
+   elif a == "Djibouti":
+    print(f"{b} {a} is Djibouti.")
+   elif a == "Equatorial Guinea":
+    print(f"{b} {a} is Malabo.")
+   elif a == "Eswatini (Swaziland)":
+    print(f"{b} {a} is Mbabane (admin.), Lobamba (royal/leg.).")
+   elif a == "Gabon":
+    print(f"{b} {a} is Libreville.")
+   elif a == "Gambia":
+    print(f"{b} {a} is Banjul.")
+   elif a == "Ghana":
+    print(f"{b} {a} is Accra.")
+   elif a == "Guinea":
+    print(f"{b} {a} is Conakry.")
+   elif a == "Guinea-Bissau":
+    print(f"{b} {a} is Bissau.")
+   elif a == "Lesotho":
+    print(f"{b} {a} is Maseru.")
+   elif a == "Liberia":
+    print(f"{b} {a} is Monrovia.")
+   elif a == "Madagascar":
+    print(f"{b} {a} is Antananarivo.")
+   elif a == "Libya":
+    print(f"{b} {a} is Tripoli.")
+   elif a == "Malawi":
+    print(f"{b} {a} is Lilongwe.")
+   elif a == "Mali":
+    print(f"{b} {a} is Bamako.")
+   elif a == "Mauritania":
+    print(f"{b} {a} is Nouakchott.")
+   elif a == "Mauritius":
+    print(f"{b} {a} is Port Louis.")
+   elif a == "Morocco":
+    print(f"{b} {a} is Rabat.")
+   elif a == "Mozambique":
+    print(f"{b} {a} is Maputo.")
+   elif a == "Namibia":
+    print(f"{b} {a} is Windhoek.")
+   elif a == "Niger":
+    print(f"{b} {a} is Niamey.")
+   elif a == "Nigeria":
+    print(f"{b} {a} is Abuja")
+   elif a == "Rwanda":
+    print(f"{b} {a} is Kigali.")
+   elif a == "São Tomé and Príncipe":
+    print(f"{b} {a} is São Tomé.")
+   elif a == "Senegal":
+    print(f"{b} {a} is Dakar.")
+   elif a == "Seychelles":
+    print(f"{b} {a} is Victoria.")
+   elif a == "Sierra Leone":
+    print(f"{b} {a} is Freetown.")
+   elif a == "South Africa":
+    print(f"{b} {a} is Pretoria (admin.), Cape Town (legis.), Bloemfontein (jud.).")
+   elif a == "South Sudan":
+    print(f"{b} {a} is Juba.")
+   elif a == "Sudan":
+    print(f"{b} {a} is Khartoum.")
+   elif a == "Togo":
+    print(f"{b} {a} is Lomé.")
+   elif a == "Tunisia":
+    print(f"{b} {a} is Tunis.")
+   elif a == "Uganda":
+    print(f"{b} {a} is Kampala.")
+   elif a == "Zambia":
+    print(f"{b} {a} is Lusaka.")
+   elif a == "Zimbabwe":
+    print(f"{b} {a} is Harare.")
+   else:
+    break
